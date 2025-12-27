ed97cb6aea0c89aed40c8f41d76665b6406184e8
Zakhar <gololobov.zahar23@gmail.com>
2025-12-27 18:22:09 +0100

    fixed api composable

--- composables/useApi.ts
+++ composables/useApi.ts
@@ -1,11 +1,16 @@
+import { useAuth } from "~/composables/useAuth"
+
 export const useApi = () => {
   const { token } = useAuth()
 
-  const apiFetch = <T>(url: string, opts: any = {}) => {
+  const apiFetch = async <T>(url: string, opts: any = {}) => {
     const headers: Record<string, string> = { ...(opts.headers || {}) }
-    if (token.value) headers.Authorization = `Bearer ${token.value}`
 
-    return $fetch<T>(url, { ...opts, headers })
+    if (token.value) {
+      headers.Authorization = `Bearer ${token.value}`
+    }
+
+    return await $fetch<T>(url, { ...opts, headers })
   }
 
   return { apiFetch }
