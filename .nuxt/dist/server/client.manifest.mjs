5e4509e083171732fabb53e33e0489c7f562eaae
Zakhar <gololobov.zahar23@gmail.com>
2026-02-08 11:54:12 +0100

    chore: remove .nuxt/ and .DS_Store from tracking, update .gitignore

--- .nuxt/dist/server/client.manifest.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///Users/Zakhar/Documents/UNI/web_systems/lendrehab/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
--- .nuxt/types/nitro-middleware.d.ts
+++
@@ -1,6 +0,0 @@
-export type MiddlewareKey = "staff"
-declare module 'nitropack' {
-  interface NitroRouteConfig {
-    appMiddleware?: MiddlewareKey | MiddlewareKey[] | Record<MiddlewareKey, boolean>
-  }
-}
